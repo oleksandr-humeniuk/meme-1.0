f838ee131cbe95f50a3344bcb702a2b914a2ce17
ohumeniuk <ohumeniuk@lohika.com>
2020-09-03 01:29:55 +0300

    Initial Commit

---
+++ Meme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by Oleksandr Humeniuk on 9/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
